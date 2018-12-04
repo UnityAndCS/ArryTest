07257e494d3fe06d6324aeff12aa471e1e1ccbdb
wyc0917 <41419630+wyc0917@users.noreply.github.com>
2018-12-04 09:00:08 +0800

    Initial commit

---
+++ ArryTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string []arry = new string[4];
+            arry[0] = "111";
+            arry[3] = "111";
+            for(int i = 0; i <=3; i++)
+            {
+                Console.WriteLine(arry[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
